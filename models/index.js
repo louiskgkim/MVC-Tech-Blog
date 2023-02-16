2881a26d549b7c4ca9043cc6d09ecf4fb1ba4a31
louiskgkim <lkgk31@gmail.com>
2023-02-15 19:05:02 -0800

    adding model files and index file with basic layout of what needs to be created

--- models/index.js
+++ models/index.js
@@ -4,3 +4,18 @@ const User = require('./User');
 const Post = require('./Post');
 // comment model
 const Comment = require('./Comment');
+
+// user post relationship
+
+// post user relationship
+
+// comment user relationship
+
+// comment post relationship
+
+// user comment relationship
+
+// post comment relationship
+
+// export modules
+module.exports = { User, Post, Comment };
