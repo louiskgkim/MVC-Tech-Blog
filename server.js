c13bfc72185bbe2a40c95e6da4ca941ff37b1bb8
louiskgkim <lkgk31@gmail.com>
2023-02-16 20:29:25 -0800

    minor add too file

--- server.js
+++ server.js
@@ -9,3 +9,19 @@ const exphbs = require('express-handlebars');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: 
+  cookie: 
+  resave: 
+  saveUninitialized: 
+  store: 
+    db:
+  })
+};
+
+// sets up express app
+const app = express();
+const PORT = process.env.PORT || 3001;
