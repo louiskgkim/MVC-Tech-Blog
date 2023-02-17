763bbfcdeb93f55c3ceedd8a151be966853ae0ea
louiskgkim <lkgk31@gmail.com>
2023-02-16 20:28:12 -0800

    start of server js file

---
+++ server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+require('dotenv').config();
+
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
