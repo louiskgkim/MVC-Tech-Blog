0d6d9d90696ed1d41c2dcf3b5e06995bcaf64124
louiskgkim <lkgk31@gmail.com>
2023-02-17 15:22:23 -0800

    adding index for models folder

--- models/index.js
+++ models/index.js
@@ -6,16 +6,42 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 
 // user post relationship
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
 
 // post user relationship
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
 
 // comment user relationship
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'cascade',
+  hooks:true
+});
 
 // comment post relationship
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'cascade',
+  hooks: true
+});
 
 // user comment relationship
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'cascade',
+  hooks:true
+});
 
 // post comment relationship
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'cascade',
+  hooks:true
+})
 
 // export modules
 module.exports = { User, Post, Comment };
