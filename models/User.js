6355a472a134cf16503170339898489e3058fd95
louiskgkim <lkgk31@gmail.com>
2023-02-17 16:00:27 -0800

    forgot bcrypt and friend helped me with hooks

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 class User extends Model {
   checkPassword(loginPw) {
@@ -46,7 +47,17 @@ User.init(
   },
   {
     hooks: {
-    
+      // friend helped me with this one
+      // set up beforeCreate lifecycle hook functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      // set up beforeUpdate lifecycle hook functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
     },
 
     sequelize,
